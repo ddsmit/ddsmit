a8df238220782adb11fa250f8a202921e53f8364
David Smit <davidsmit@Davids-MacBook-Air.local>
2023-05-06 23:02:14 -0400

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-		  external: [            
-			'fsevents'
-		  ]
-	  }
-	}
+	plugins: [sveltekit()]
+	// build: {
+	// 	rollupOptions: {
+	// 	  external: [            
+	// 		'fsevents'
+	// 	  ]
+	//   }
+	// }
 };
 
 export default config;
