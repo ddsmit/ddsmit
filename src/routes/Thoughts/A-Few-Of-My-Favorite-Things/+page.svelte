683a64a549a89296c61b2b7a8327d00ef1a828fe
David Smit <davidsmit@Davids-MacBook-Air.local>
2023-12-07 02:16:56 -0500

    Adding a favorites page

---
+++ src/routes/Thoughts/A-Few-Of-My-Favorite-Things/+page.svelte
@@ -0,0 +1,22 @@
+<h1>These are a few of my favorite things.</h1>
+<p>
+    A place for me to dump some of my favorite media, libraries, articles, etc. 
+</p>
+<h2>Podcasts</h2>
+<ul>
+    <li>
+        <a href="https://www.jupiterbroadcasting.com/show/coder-radio/">Coder Radio</a>
+    </li>
+    <li>
+        <a href="https://www.codingblocks.net/">Coding Blocks</a>
+    </li>
+    <li>
+        <a href="https://syntax.fm/">Syntax</a>
+    </li>
+    <li>
+        <a href="https://pythonbytes.fm/">Python Bytes</a>
+    </li>
+    <li>
+        <a href="https://talkpython.fm/">Talk Python</a>
+    </li>
+</ul>
