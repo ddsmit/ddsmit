8ba62dda6556c0583fdfc9c4347fb2a8bf2037e6
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-07-16 02:33:30 -0400

    Added domain

--- src/routes/api/metadata/+server.js
+++ src/routes/api/metadata/+server.js
@@ -1,7 +1,7 @@
 import urlMetadata from 'url-metadata';
 
 const allowedOrigin = {
-  'Access-Control-Allow-Origin': 'http://localhost:5173'
+  'Access-Control-Allow-Origin': ['https://ddsmit.com','https://www.ddsmit.com']
 };
 
 export async function GET (req) {
