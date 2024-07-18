027f8b993f47f5c87cc2cb01775e3a3174efc9e8
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-07-17 21:16:06 -0400

    test

--- src/routes/ReadList/+page.ts
+++ src/routes/ReadList/+page.ts
@@ -8,7 +8,7 @@ export const load = (async () => {
 		const readListWithMetadata = await Promise.all(
 			readList.map(async (readitem) => {
 				// const response = await fetch(`http://localhost:5173/api/metadata?url=${encodeURIComponent(readitem.link)}`);
-				const metadata = await response.json();
+				// const metadata = await response.json();
 				return {
 					...readitem,
 					// metadata
