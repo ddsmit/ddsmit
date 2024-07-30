e9b63eac01b2358c76958e8142780e49f8f383d2
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-07-29 20:00:07 -0400

    Deleting standard pages replaces by boards

--- src/routes/Thoughts/Articles/Tech-Wish-List/+page.svelte
+++
@@ -1,6 +0,0 @@
-<h2>What do I wish?</h2>
-<p>
-    This area of my thoughts is a way to jot down some things I think of that would
-    be great to have in some of the languages and tools I use most often, as soon as I think of them.
-    Eventually, I will submit peps, requests, etc to address the concerns.
-</p>
